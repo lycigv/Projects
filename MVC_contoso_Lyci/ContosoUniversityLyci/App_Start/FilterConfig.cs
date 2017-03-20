4a1601e4e3047749b449f0aac4a934d2b3be00e9
Lyci <lycigv@gmail.com>
2017-03-20 13:37:19 -0700

    Demo

---
+++ MVC_contoso_Lyci/ContosoUniversityLyci/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversityLyci
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
